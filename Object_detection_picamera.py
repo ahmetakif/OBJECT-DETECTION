031949862cfb47161b3c9f8dc234a0f11e1a7130
Ahmet Akif Kaya <ahmetakifkaya11@gmail.com>
2020-02-23 23:39:21 +0300

    Update Object_detection_picamera.py

--- Object_detection_picamera.py
+++ Object_detection_picamera.py
@@ -19,10 +19,18 @@
 ## https://github.com/datitran/object_detector_app/blob/master/object_detection_app.py
 
 # This code is specifically written for the Vision-Bilgegoz project by Ahmet Akif Kaya
-# and all rights of this program is owned by Ahmet Akif Kaya under MIT open source license. 
-# You are automatically accepting the terms of the MIT license by using this code in any way.
-# The terms and conditions about this project's MIT license can be found in the file named
+# and all rights of this program is owned by Ahmet Akif Kaya under APACHE open source license. 
+# You are automatically accepting the terms of the APACHE license by using this code in any way.
+# The terms and conditions about this project's APACHE license can be found in the file named
 # "LICENSE" in the root directory of this repository.
+
+# Copyright 2020 Ahmet Akif Kaya
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. 
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific 
+# language governing permissions and limitations under the License.
 
 import os
 import cv2
